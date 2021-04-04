58fc17a6e5373d735afc16c66e77d4cd95907f9a
Michael Dean <mdean@nrtc.coop>
2021-04-04 12:47:06 -0500

    first

---
+++ ncm-control/ncm_control.h
@@ -0,0 +1,14 @@
+//
+//  ncm_control.h
+//  ncm-control
+//
+//  Created by Michael Dean on 4/4/21.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface ncm_control : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ ncm-control/ncm_control.m
@@ -0,0 +1,16 @@
+//
+//  ncm_control.m
+//  ncm-control
+//
+//  Created by Michael Dean on 4/4/21.
+//
+
+#import "ncm_control.h"
+
+@implementation ncm_control
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
